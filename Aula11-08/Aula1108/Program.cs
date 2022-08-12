47172fe0cd7033963108cfe5aa2c02355bd5ef94
Tales Bacarin Lopes <71499200+talesbaca@users.noreply.github.com>
2022-08-11 21:55:21 -0300

    Calcula Area 11/08
    Pequeno exemplo da aula1 11/08

---
+++ Aula11-08/Aula1108/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Aula1108
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe uma altura: ");
+            string altura = Console.ReadLine();
+            Console.WriteLine("Informe uma largura: ");
+            string largura = Console.ReadLine();
+            Console.WriteLine("Area do retângulo: "  + AreaCompute(Double.Parse(altura), Double.Parse(largura)));
+        }
+
+        static double AreaCompute(double altura, double largura)
+        {
+            double area = altura * largura;
+            return area;
+        } 
+    }
+}

bd0f939548507e7ef275821239f75db455f82823
Tales Bacarin Lopes <71499200+talesbaca@users.noreply.github.com>
2022-08-18 21:46:30 -0300

    Aula 18/08(Média,mediana e moda)

---
+++ Aula18-08/aula18-08/Program.cs
@@ -0,0 +1,73 @@
+﻿//
+using System;
+
+namespace lista1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora de média, mediana e moda de um Array");
+            double[] arr = {123, 412, 543 , 7567, 3,500,1234 };
+            Console.WriteLine("Média: " + calculaMedia(arr));
+            Console.WriteLine("Mediana: " + calculaMediana(arr));
+            if(calculaModa(arr) == 0){
+                Console.WriteLine("Moda: Não possui moda");
+            }else{
+                Console.WriteLine("Moda: " + calculaModa(arr));
+            }
+            
+            
+        }
+
+        static double calculaMedia(double[] nums){
+            double media = 0;
+            for(int i=0;i < nums.Length; i++){
+                media += nums[i];
+            }
+            media = media/nums.Length;
+            return media;
+        }
+
+         static double calculaMediana(double[] nums){
+            double mediana = 0;
+            bool isImpar = false; 
+            Array.Sort(nums);
+            if(nums.Length % 2 != 0){
+                isImpar = true;
+            }
+
+            if(isImpar){
+                int posicao = (nums.Length + 1 ) / 2;
+                mediana = nums[posicao -1];
+            }else{
+                int posicao1 = (nums.Length / 2);
+                int posicao2 = (nums.Length / 2) + 1;
+
+                mediana = (nums[posicao1 - 1] + nums[posicao2 - 1]) / 2;
+            }
+            return mediana;
+        }    
+
+         static double calculaModa(double[] nums){
+            double moda = 0;
+            double conta = 0;
+            double[] auxV = new double[nums.Length];
+            for(int i=0;i<nums.Length;i++){
+                for(int j=i+1;j<nums.Length;j++){
+                    
+                    if(nums[i] == nums[j]){
+                        auxV[i]++;
+                            if(auxV[i]>conta){
+                                conta=auxV[i];
+                                moda=nums[i];
+                            }
+                    }
+                    
+                }
+                auxV[i]=0;
+            }
+            return moda;
+        }
+    }
+}
